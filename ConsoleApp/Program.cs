f6d84f101e46f9500394f31bbcea506b0100e240
Piedrah <luispiedrahita1131209@correo.itm.edu.co>
2025-08-21 09:22:02 -0500

    Clases básicas

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Equipos
+{
+    public int Id;
+    public String? Nombre;
+
+}
+
+public class Jugadores
+{
+    public int Id;
+    public String? Nombre;
+    public decimal Estatura;
+    public bool Activo;
+    public Equipos? Equipo;
+
+}
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f413482f3c55b10a92cd4281b242fa92036630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccc880b10f3dc9b899c8ab2513edf8c05be362c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
